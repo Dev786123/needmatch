dd838336e852d536f5484ec371460ca10b945d57
Dev Rathod <dr2885537@gmail.com>
2026-04-24 21:54:49 +0530

    add middleware structure

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
           <img
             src="/logo.png"
             alt="NeedMatch"
-            className="h-11 w-auto object-contain"
+            className="h-15 w-auto object-contain"
           />
         </a>
 
